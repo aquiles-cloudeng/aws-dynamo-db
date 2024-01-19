63d0c26b83a29446f40d1c535d55dc3a72f2592a
Aquiles Calvo <ascalvo@teco.com.ar>
2024-01-19 14:40:54 -0300

    update to v5

--- versions.tf
+++ versions.tf
@@ -1,9 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+      source = "hashicorp/aws"
+      version = "~> 5.0"
     }
-  }
+    archive = {
+      source = "hashicorp/archive"
+      version = "2.4.1"
+    }
+  }  
   required_version = ">= 0.13"
 }
